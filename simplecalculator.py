0b13c965f6b4a1315dbd5d06675b4d52818b7438
Wardan Nugraha Ahmad <160828737+wardannugraha@users.noreply.github.com>
2024-05-26 19:48:53 +0700

    Add files via upload

---
+++ simplecalculator.py
@@ -0,0 +1,48 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ['1', '2', '3', '4']:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"{num1} + {num2} = {add(num1, num2)}")
+
+        elif choice == '2':
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
+
+        elif choice == '3':
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+        elif choice == '4':
+            result = divide(num1, num2)
+            if result == "Error! Division by zero.":
+                print(result)
+            else:
+                print(f"{num1} / {num2} = {result}")
+    else:
+        print("Invalid Input")
+
+# Run the calculator function
+calculator()
